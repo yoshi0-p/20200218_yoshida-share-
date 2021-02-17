4220c78b3ef7df014b307598f73a6035ecde2829
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-02-17 17:21:42 +0900

    修正内容が分かるコミットメッセージ

--- src/router/index.js
+++ src/router/index.js
@@ -43,21 +43,5 @@ const router = new VueRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  if (
-    to.matched.some((record) => record.meta.requiresAuth) &&
-    !store.state.auth
-  ) {
-    next({
-      path: "/",
-      query: {
-        redirect: to.fullPath,
-      },
-    });
-  } else {
-    next();
-  }
-});
-
 
 export default router;
