3732eec1892bd6a7a6c9597b34ad3e553b8dc311
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-06 12:03:51 +0900

    second-commit

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -152,7 +152,7 @@ export default {
     },
 
      comment() {
-       this.shares = shares;
+       shares = this.shares;
         for(let x = 0; x <shares.length; x++){
         axios
         .get("https://calm-atoll-21933.herokuapp.com/api/shares/" + shares[x].id)
@@ -160,6 +160,7 @@ export default {
           this.shares[x].comment = response.data.comment;
         });  
         }
+        this.shares = shares;
     },
 
   },
