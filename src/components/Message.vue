171cca73d5b50771715ce4133d9c8872ce27c476
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-07 10:55:06 +0900

    second-commit

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -25,6 +25,11 @@
           >[More]</p>
         </div>
         <p class="text">{{ value.item.share }}</p>
+       <input v-model="content" type="text" />
+        <div @click="send(index)">
+          <button>コメント</button>
+        </div> 
+
          <div class="message" v-for="(comments, index) in value.comment" :key="index">
            <div class="flex">
             <p class="name">{{ comments.comment_user.name }}</p>
@@ -33,10 +38,7 @@
             <p class="text">{{ comments.comment.content }}</p>
            </div>
           </div>
-       <!--  <input v-model="content" type="text" />
-        <div @click="send(index)">
-          <button>コメント</button>
-        </div> -->
+       
   
       </div>
     </div>
@@ -60,7 +62,7 @@ export default {
   methods: {
      
 
- /*    send(index) {
+    send(index) {
       axios
         .post("https://calm-atoll-21933.herokuapp.com/api/comment", {
           share_id: this.shares[index].item.id,
@@ -75,7 +77,7 @@ export default {
             force: true,
           });
         });
-    }, */
+    }, 
   
     fav(index) {
       const result = this.shares[index].like.some((value) => {
