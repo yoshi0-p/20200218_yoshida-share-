503a5fa0b5646323a7c810b6fed183de34314fc2
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-07 11:26:57 +0900

    second-commit

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -201,7 +201,7 @@ export default {
 .message {
   margin: 20px;
   background-color:rgb(255, 144, 231);
-  width:50vw;
+  width:30vw;
   border-bottom: solid 1px red;
   border-left: solid 1px red;
 
@@ -223,7 +223,7 @@ export default {
   margin-left:50px;
   padding:20px;
   background-color:rgb(144, 229, 255);
-  width:50vw;
+  width:30vw;
   border-bottom: solid 1px aquamarine;
   border-left: solid 1px aquamarine;
 
