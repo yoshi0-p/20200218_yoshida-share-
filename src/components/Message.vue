43884ed72e855f0ae1952824b64d40584b90135c
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-07 10:53:09 +0900

    second-commit

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -25,7 +25,6 @@
           >[More]</p>
         </div>
         <p class="text">{{ value.item.share }}</p>
-　　　　
          <div class="message" v-for="(comments, index) in value.comment" :key="index">
            <div class="flex">
             <p class="name">{{ comments.comment_user.name }}</p>
